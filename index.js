ae2944978401cc632a2a695b3ff7a0357c814fc3
Pete Diemert <pete@boundary.com>
2014-07-19 19:05:54 -0700

    nzb

--- index.js
+++ index.js
@@ -4,22 +4,44 @@ process.on('uncaughtException', function(err)
 	process.exit(0);
 });
 
-var _applescript = require('applescript');
 var _conf = global.conf = require('./config.json');
+
+var _applescript = require('applescript');
 var _pkg = require('./package.json');
 var _logger = require('./lib/logger');
 var _helper = require('./lib/helper');
 var _watchers = require('./lib/watchers');
 var _db = require('./lib/db');
 
-_logger.info('SSTV v%s using Node v%s', _pkg.version, process.version);
+_logger.info('SSTV v%s using Node %s', _pkg.version, process.version);
 
-if (process.argv[2] == 'wipe')
+
+var _opt = require('optimist');
+
+var _argv = _opt
+	.usage('A stupid simple home media center')
+	.options('w', { alias : 'webvideo', describe : 'only update the web video source specified' })
+	.options('!', { alias : 'wipe', describe : 'wipes the database on startup' })
+	.options('h', { alias : 'help', describe : 'show this message' })
+	.argv;
+
+if (_argv.help)
+{
+	_opt.showHelp();
+	process.exit(0);
+}
+
+if (_argv.wipe)
 {
 	_conf.wipe_db = true;
 	_logger.info('Wiping database');
 }
 
+if (_argv.webvideo)
+{
+	_conf.update_only = {webvideo : _argv.webvideo};
+	_logger.info('Only updating webvideo %s', _argv.webvideo);
+}
 var app = require('./app');
 
 app.set('port', process.env.PORT || 3000);
